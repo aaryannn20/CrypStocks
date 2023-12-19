e6aa427b65fee952585e14a16321b300892ed2ae
pg147 <xyzprathamesh147@gmail.com>
2023-12-19 11:46:22 +0530

    Minor event changes added

--- app/src/main/java/com/starorigins/crypstocks/ui/screens/userauth/LoginScreen.kt
+++ app/src/main/java/com/starorigins/crypstocks/ui/screens/userauth/LoginScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.starorigins.crypstocks.R
 import com.starorigins.crypstocks.ui.components.DirectLoginFacilityRow
 import com.starorigins.crypstocks.ui.components.DividerComponent
@@ -21,7 +22,7 @@ import com.starorigins.crypstocks.ui.components.PasswordField
 import com.starorigins.crypstocks.ui.components.RouteSignUpScreen
 
 @Composable
-fun LoginScreen() {
+fun LoginScreen(loginViewModel : UserAuthViewModel = viewModel()) {
     Surface(modifier = Modifier
         .fillMaxSize()
     ) {
@@ -29,11 +30,27 @@ fun LoginScreen() {
             Spacer(modifier = Modifier.height(10.dp))
             LogoContainer(image = painterResource(id = R.drawable.logo))
             Spacer(modifier = Modifier.height(32.dp))
-            MyTextField(labelValue = "Username", leadIcon = painterResource(id = R.drawable.profile))
+            MyTextField(
+                labelValue = "Username",
+                leadIcon = painterResource(id = R.drawable.profile),
+                onInput = {
+                    loginViewModel.onEvent(UserAuthEvents.UserNameChanged(it))
+                }
+            )
             Spacer(modifier = Modifier.height(28.dp))
-            PasswordField(labelValue = "Password", leadIcon = painterResource(id = R.drawable.password))
+            PasswordField(
+                labelValue = "Password",
+                leadIcon = painterResource(id = R.drawable.password),
+                onInput = {
+                    loginViewModel.onEvent(UserAuthEvents.PasswordChanged(it))
+                }
+            )
             Spacer(modifier = Modifier.height(24.dp))
-            LoginButton()
+            LoginButton(
+                onButtonClicked = {
+                    loginViewModel.onEvent(UserAuthEvents.LoginButtonClicked)
+                }
+            )
             Spacer(modifier = Modifier.height(16.dp))
             RouteSignUpScreen()
             Spacer(modifier = Modifier.height(32.dp))
